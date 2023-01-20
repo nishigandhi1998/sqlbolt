7612bb20c08e1d459ab9002e5e0e18cb6f101d52
Nishi Gandhi <72174760+nishigandhi1998@users.noreply.github.com>
2023-01-20 12:46:32 +0530

    Update sqlbolt_ans.sql

--- sqlbolt_ans.sql
+++ sqlbolt_ans.sql
@@ -346,11 +346,12 @@ Create Table Database (
 
 -- Add a column named Aspect_ratio with a FLOAT data type to store the aspect-ratio each movie was released in.
 ALTER TABLE Movies
-  ADD COLUMN Aspect_ratio FLOAT DEFAULT 3;
+ADD Aspect_ratio Float;
   
 -- Add another column named Language with a TEXT data type to store the language that the movie was released in. Ensure that the default for this language is English.
 ALTER TABLE Movies
-  ADD COLUMN Language TEXT DEFAULT "English";
+ADD Language TEXT
+ default "English";
 
 -- CH18 - Dropping Tables
 
